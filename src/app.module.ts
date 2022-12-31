b42c6e93d1a2af0d51dcff36480fb1b8dd0e3112
alahirajeffrey <alahirajeffrey@gmail.com>
2022-12-31 19:46:23 +0300

    added imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KnexModule } from 'nest-knexjs';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    KnexModule.forRoot({
+      config: {
+        client: 'mysql',
+        useNullAsDefault: true,
+        connection: {
+          host: process.env.DEV_DB_HOST,
+          user: process.env.DEV_DB_USER,
+          password: process.env.DEV_DB_PASSWORD,
+          database: process.env.DEV_DB_DATABASE,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
