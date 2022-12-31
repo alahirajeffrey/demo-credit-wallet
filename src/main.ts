b2b631784a190f0a2f69c88bb63c7d095845b86f
alahirajeffrey <alahirajeffrey@gmail.com>
2022-12-31 19:21:32 +0300

    setup swagger and swagger documentation

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Demo Credit Wallet')
+    .setDescription('The Demo credit wallet api')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
