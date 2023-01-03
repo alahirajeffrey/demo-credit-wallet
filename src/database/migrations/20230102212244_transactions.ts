24837891ea93eae01eb347d9f9cc5c74ae7d4cf0
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-03 08:27:38 +0100

    setup transaction table

---
+++ src/database/migrations/20230102212244_transactions.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('transactions', (table) => {
+    table.increments('id').primary(),
+      table
+        .enum('transactionType', ['transfer', 'withdrawal'])
+        .defaultTo('withdrawal'),
+      table.float('amount'),
+      table.string('recieverEmail'),
+      table.boolean('isTransactionSuccessful'),
+      table.string('accountEmail').references('userEmail').inTable('accounts'),
+      table.timestamp('transactionTime').defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {}
