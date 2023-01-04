442586051b5fb6d108effb6b2efd0a93be47f25d
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-04 15:47:58 +0100

    added dto to withdraw funds

--- src/transactions/dto/withdraw-funds.dto.ts
+++ src/transactions/dto/withdraw-funds.dto.ts
@@ -1,1 +1,13 @@
-export class WithdrawFundsDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class WithdrawFundsDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  amount: number;
+
+  @IsEmail()
+  @ApiProperty()
+  @IsNotEmpty()
+  accountEmail: string;
+}
