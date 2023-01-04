739f86e8e578e6548dbd32749a1863872dc1278d
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-04 15:47:47 +0100

    added dto to transafer funds

--- src/transactions/dto/transfer-funds.dto.ts
+++ src/transactions/dto/transfer-funds.dto.ts
@@ -1,1 +1,18 @@
-export class TransferFundsDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class TransferFundsDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  amount: number;
+
+  @IsEmail()
+  @ApiProperty()
+  @IsNotEmpty()
+  accountEmail: string;
+
+  @IsEmail()
+  @ApiProperty()
+  @IsNotEmpty()
+  recieverEmail: string;
+}
