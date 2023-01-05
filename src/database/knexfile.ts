44baa349434acb6f26357c5771b8a5d015297c88
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-05 20:45:53 +0100

    code refactor

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -1,13 +1,16 @@
 import { Knex } from 'knex';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'mysql',
     connection: {
-      host: process.env.DEV_DB_HOST || '127.0.0.1',
-      user: process.env.DEV_DB_USER || 'root',
-      password: process.env.DEV_DB_PASSWORD || '102296',
-      database: process.env.DEV_DB_DATABASE || 'demo-credit-wallet',
+      host: '127.0.0.1',
+      user: 'root',
+      password: '102296',
+      database: 'demo-credit-wallet',
     },
     debug: true,
   },
@@ -15,11 +18,11 @@ const config: { [key: string]: Knex.Config } = {
   production: {
     client: 'mysql',
     connection: {
-      host: process.env.DB_HOST,
       user: process.env.DB_USER,
+      host: process.env.DB_HOST,
       password: process.env.DB_PASWWORD,
       database: process.env.DB_DATABASE,
-      port: Number(process.env.PORT),
+      port: Number(process.env.DB_PORT),
     },
     pool: {
       min: 2,
@@ -27,6 +30,7 @@ const config: { [key: string]: Knex.Config } = {
     },
     migrations: {
       tableName: 'knex_migrations',
+      // directory: './migrations',
     },
   },
 };
